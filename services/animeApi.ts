f34af3f710b093a85542b507f0cdbb766da2d6c3
J94rubio <jb.rubio@outlook.com>
2026-05-08 19:23:42 -0500

    Fix api url for url of api on Render

--- services/animeApi.ts
+++ services/animeApi.ts
@@ -29,7 +29,8 @@ export interface AnimeDetail {
   extra_data?: string[];
 }
 
-const API_BASE_URL = "http://127.0.0.1:5000";
+// const API_BASE_URL = "http://127.0.0.1:5000";
+const API_BASE_URL = "https://anime-api-3upj.onrender.com";
 
 export async function searchAnime(query: string): Promise<AnimeDetail[]> {
   try {
